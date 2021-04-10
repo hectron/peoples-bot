f78274a7f71b75a998ff389f3cb632905b7dbdb5
Hector Rios <that.hector@gmail.com>
2021-04-10 00:12:57 -0500

    Clean up how output is done (#5)

--- vaccine_spotter_result.rb
+++ vaccine_spotter_result.rb
@@ -1,14 +1,31 @@
 class VaccineSpotterResult
+  MESSAGE_CHARACTER_LIMIT = 2_000
+
   # @param locations [Array<Location>]
   def self.display(locations)
     return "Did not find any appointments." if locations.none?
 
-    locations.map do |location|
-      msg = <<~MSG.strip
-        Found #{location.appointments.size} appointment(s) for the #{location.vaccine_types.join(" and ")} vaccine at #{location.name} (#{location.provider}) - #{location.city}, #{location.state} #{location.postal_code}
+    total_appointments = locations.sum { |location| location.appointments.size }
+    output = if total_appointments.size == 1
+               "Found a total of #{total_appointments} appointment!"
+             else
+               "Found a total of #{total_appointments} appointments!"
+             end
 
+    locations.each do |location|
+      msg = <<~MSG.strip
+        #{location.appointments.size} appointment(s) for the #{location.vaccine_types.join(" and ")} vaccine at #{location.name} (#{location.provider}) - #{location.city}, #{location.state} #{location.postal_code}
         Appointment URL: #{location.url}
       MSG
-    end.join("\n\n")
+
+      if (output + msg).size < MESSAGE_CHARACTER_LIMIT
+        output += "\n\n"
+        output += msg
+      else
+        break
+      end
+    end
+
+    output
   end
 end
