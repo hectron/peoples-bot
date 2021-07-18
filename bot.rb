f771d4d63b5fb3d4711216a70971dfed94f93e8b
Hector Rios <that.hector@gmail.com>
2021-07-18 14:42:43 -0500

    Add rubocop + clean up bot

--- bot.rb
+++ bot.rb
@@ -5,12 +5,14 @@ require_relative "./app/discord/command"
 require_relative "./app/vaccine_spotter/api"
 require_relative "./app/vaccine_spotter/result"
 
-LOGGER = Logger.new(STDOUT)
+LOGGER = Logger.new($stdout)
 LOGGER.level = Logger::INFO
 
-bot = Discordrb::Commands::CommandBot.new(token: ENV["DISCORD_BOT_CLIENT_TOKEN"],
-                                          client_id: ENV["DISCORD_BOT_CLIENT_ID"],
-                                          prefix: BOT_COMMAND_PREFIX)
+bot = Discordrb::Commands::CommandBot.new(
+  token: ENV["DISCORD_BOT_CLIENT_TOKEN"],
+  client_id: ENV["DISCORD_BOT_CLIENT_ID"],
+  prefix: BOT_COMMAND_PREFIX,
+)
 
 VACCINE_TYPES.each do |type|
   command_config = {
@@ -28,16 +30,15 @@ VACCINE_TYPES.each do |type|
   }
 
   bot.command(type.to_sym, command_config) do |_event, state, *args|
-    LOGGER.info <<~INFO
-      Command type: #{type}, State: #{state}, Args: #{args.inspect}
-      Event: #{_event.inspect}
-    INFO
+    LOGGER.info "Command type: #{type}, State: #{state}, Args: #{args.inspect}"
 
     command = Discord::Command.new(args)
-    locations = VaccineSpotter::Api.find_in(state: state,
-                                            vaccine_type: type,
-                                            city: command.city,
-                                            zipcodes: command.zipcodes)
+    locations = VaccineSpotter::Api.find_in(
+      state: state,
+      vaccine_type: type,
+      city: command.city,
+      zipcodes: command.zipcodes,
+    )
 
     VaccineSpotter::Result.display(locations).tap do |msg|
       LOGGER.info msg
