d93c519c5ff7457eb38b1bb26cc3f83d895404c2
Hector Rios <that.hector@gmail.com>
2021-07-20 09:34:36 -0500

    Add beach sand (#11)
    * Add BeachSand to bot.
    
    * Add BeachSand to application
    
    * Add specs for discordmessage
    
    * Add specs for message deleter
    
    * Don't need between_time?

---
+++ app/beach_sand/structs/discord_message.rb
@@ -0,0 +1,26 @@
+require "date"
+require "time"
+
+module BeachSand
+  module Structs
+    # 60 seconds, for 60 minutes, 24 times a day, 7 days in a week, for two weeks
+    TwoWeeksAgo = 60 * 60 * 24 * 7 * 2
+
+    DiscordMessage = Struct.new(:id, :author_id, :timestamp, keyword_init: true) do
+      # @return [Array[DiscordMessage]]
+      def self.from_api_response(response)
+        response.map do |msg|
+          new(
+            id: msg["id"],
+            author_id: msg.dig("author", "id"),
+            timestamp: DateTime.parse(msg["timestamp"]),
+          )
+        end
+      end
+
+      def deletable?
+        timestamp.to_time.to_i > (Time.now.to_i - TwoWeeksAgo)
+      end
+    end
+  end
+end
--- bot.rb
+++ bot.rb
@@ -4,6 +4,7 @@ require_relative "./app/constants"
 require_relative "./app/discord/command"
 require_relative "./app/vaccine_spotter/api"
 require_relative "./app/vaccine_spotter/result"
+require_relative "./app/beach_sand"
 
 LOGGER = Logger.new($stdout)
 LOGGER.level = Logger::INFO
@@ -18,7 +19,7 @@ VACCINE_TYPES.each do |type|
   command_config = {
     help_available: true,
     description: "Find #{type} vaccines",
-    usage: <<~USAGE.strip,
+    usage: <<~USAGE.strip
       !#{type} <STATE> [<CITY> (optional)] <zipcode1> <zipcode2> ... <zipcodeN>
 
       Examples:
@@ -43,6 +44,30 @@ VACCINE_TYPES.each do |type|
     VaccineSpotter::Result.display(locations).tap do |msg|
       LOGGER.info msg
     end
+  end
+end
+
+bot.command(
+  :beach,
+  help_available: true,
+  description: "Primes the sands for temporary messaging.",
+  usage: "React to the first message the bot posted with :ocean: to cause a deluge",
+) do |event|
+  msg = event.response "The waves subside. For a brief moment, you can write in sand before then next tide. When you are done, react to the bot message with :ocean:."
+  done_emoji = "\u{1f30a}"
+
+  msg.react(done_emoji)
+
+  bot.add_await!(Discordrb::Events::ReactionAddEvent, emoji: done_emoji, timeout: 600, message: msg) do |reaction_event|
+    deleter = BeachSand::MessageDeleter.new(message_id: msg.idd, channel_id: msg.channel.id, api_token: bot.token)
+
+    begin
+      deleter.execute
+    rescue NoMessagesError, ArgumentError => e
+      LOGGER.error(e)
+    end
+
+    reaction_event.respond "The tides roll in, and the sand begins to smooth out."
   end
 end
 
