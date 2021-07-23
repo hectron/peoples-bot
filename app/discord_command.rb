c8672a96fea005b5e35c07d5a75ffb593b153b1e
Hector Rios <that.hector@gmail.com>
2021-07-23 14:35:04 -0500

    Make state includable

--- app/discord_command.rb
+++ app/discord_command.rb
@@ -1,4 +1,45 @@
+# Creates a shell that allows us to create a DSL
+module AbstractDiscordCommand
+  def included
+    @event_handler = nil
+    @name = nil
+    @description = nil
+    @usage = nil
+    @help_available = false
+  end
+
+  def event_handler(handler = nil)
+    @event_handler ||= handler
+  end
+
+  def name(name = nil)
+    @name ||= name
+  end
+
+  def description(description = nil)
+    @description ||= description
+  end
+
+  def usage(usage = nil)
+    @usage ||= usage
+  end
+
+  def help_available
+    !!(usage || description)
+  end
+
+  def config
+    {}.tap do |conf|
+      conf[:description] = description if description
+      conf[:usage] = usage if usage
+      conf[:help_available] = help_available
+    end
+  end
+end
+
 class DiscordCommand
+  extend AbstractDiscordCommand
+
   def initialize(bot)
     @bot = bot
   end
@@ -6,34 +47,6 @@ class DiscordCommand
   def register
     @bot.command(self.class.name, self.class.description, self.class.config) do |*args|
       self.class.event_handler.call(args)
-    end
-  end
-
-  def self.event_handler(handler = nil)
-    @@event_handler ||= handler
-  end
-
-  def self.name(name = nil)
-    @@name ||= name
-  end
-
-  def self.description(description = nil)
-    @@description ||= description
-  end
-
-  def self.usage(usage = nil)
-    @@usage ||= usage
-  end
-
-  def self.help_available
-    !!(usage || description)
-  end
-
-  def self.config
-    {}.tap do |conf|
-      conf[:description] = description if description
-      conf[:usage] = usage if usage
-      conf[:help_available] = help_available
     end
   end
 end
