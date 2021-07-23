e184a23ff7ed014dbcc0901bda36c184c98c561b
Hector Rios <that.hector@gmail.com>
2021-07-23 14:03:19 -0500

    Use command pattern to create registerable discord commands

--- app/beach_sand/discord_command.rb
+++ app/beach_sand/discord_command.rb
@@ -1,41 +1,30 @@
 require "set"
 
 module BeachSand
-  class DiscordCommand
-    @@active_sessions = Set.new([])
-
+  class DiscordCommand < ::DiscordCommand
+    ActiveSessions = Set.new([])
     DoneEmoji = "\u{1f30a}".freeze
 
-    def initialize(bot)
-      @bot = bot
-    end
+    name :beach
+    description "Primes the sands for temporary messaging.".freeze
+    usage "React to the bot's original message with :ocean: to cause a deluge.".freeze
 
-    def register
-      @bot.command(self.class.name,
-                   self.class.description,
-                   help_available: true,
-                   self.class.config) do |event|
-        handle(event)
-      end
-    end
-
-    def handle(event)
-      unless @@active_sessions.add?(event.channel.id)
-        next "There is a current beach session in progress. This will be ignored until then."
+    event_handler ->(event) do
+      unless ActiveSessions.add?(event.channel.id)
+        return "There is a current beach session in progress. This will be ignored until then."
       end
 
       msg = event.respond "The waves subside. For a brief moment, you can write in sand before then next tide. When you are done, react to the bot message with :ocean:."
-      done_emoji = "\u{1f30a}"
 
       msg.react(DoneEmoji)
 
-      bot.add_await!(Discordrb::Events::ReactionAddEvent, emoji: DoneEmoji, timeout: 600, message: msg) do |reaction_event|
+      @bot.add_await!(Discordrb::Events::ReactionAddEvent, emoji: DoneEmoji, timeout: 600, message: msg) do |reaction_event|
         # Since this code will run on every :ocean: reaction, it might not
         # be on our time message we sent earlier. We use `next` to skip the rest
         # of the block unless it was our message that was reacted to.
         next true unless reaction_event.message.id == msg.id
 
-        deleter = BeachSand::MessageDeleter.new(message_id: msg.id, channel_id: msg.channel.id, api_token: bot.token)
+        deleter = BeachSand::MessageDeleter.new(message_id: msg.id, channel_id: msg.channel.id, api_token: @bot.token)
 
         begin
           msg.delete
@@ -44,34 +33,14 @@ module BeachSand
           BobLog.error(e)
         end
 
-        @@active_sessions.delete?(msg.channel.id)
+        ActiveSessions.delete?(msg.channel.id)
 
         reaction_event.respond "The tides roll in, and the sand begins to smooth out."
       end
 
+      ActiveSessions.delete?(msg.channel.id)
+
       nil
-    end
-
-    class << self
-      def name
-        :beach
-      end
-
-      def description
-        "Primes the sands for temporary messaging.".freeze
-      end
-
-      def usage
-        "React to the bot's original message with :ocean: to cause a deluge.".freeze
-      end
-
-      def config
-        {}.tap do |conf|
-          conf[:description] = description
-          conf[:usage] = usage
-        end
-      end
-
     end
   end
 end
