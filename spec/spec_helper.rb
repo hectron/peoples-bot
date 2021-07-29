2ddd7ef42c95d778a7f2fe06166c3ed357d6e5d3
Hector Rios <that.hector@gmail.com>
2021-07-28 22:46:19 -0500

    Use docker (#29)
    * Add Docker, docker-compose and dip
    
    * Add stub_env for easier testing
    
    * Clean up redis implementation
    
    * Set redis url for ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require "stub_env"
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -101,4 +103,6 @@ RSpec.configure do |config|
     logger.device = StringIO.new
     logger.level = Logger::WARN
   end
+
+  config.include StubEnv::Helpers
 end
