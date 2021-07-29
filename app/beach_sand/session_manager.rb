115b10460a7d1b199ae3420dd09d075afcb780df
Hector Rios <that.hector@gmail.com>
2021-07-28 21:45:06 -0500

    Use a distributed lock store for keeping track of active sessions (#27)
    * Add Redlock to the application
    
    This commit adds Redlock, which is a Redis gem that allows us to use a
    distributed lock using Redis.
    
    * Add redlock to the session manager interface
    
    This commit adds the ability to conditionally use redlock, if it is
    enabled.
    
    * Use BeachSand::SessionManager to expire sessions
    
    * Do not use TLS redis on Heroku

--- app/beach_sand/session_manager.rb
+++ app/beach_sand/session_manager.rb
@@ -1,14 +1,32 @@
 require "set"
+require "redlock"
 
 module BeachSand
   class SessionManager
+    class BadTimeout < StandardError; end
+
+    MaxTimeoutAllowed = 1_000 * 60 * 60 # 1 hour, in milliseconds
+    MinTimeoutAllowed = 1_000 * 60 # 1 minute, in milliseconds
+
     class << self
-      def acquire_lock(lock_name)
-        !!session_store.add?(lock_name)
+      def acquire_lock(lock_name, timeout: nil)
+        if redlock_enabled?
+          raise BadTimeout, "Timeout must be provided (in milliseconds)" if timeout.nil?
+          raise BadTimeout, "Timeout must be at least #{MinTimeoutAllowed / 1_000} seconds" if timeout < MinTimeoutAllowed
+          raise BadTimeout, "Timeout must be at most #{MaxTimeoutAllowed / 1_000} seconds" if timeout > MaxTimeoutAllowed
+
+          redlock_client.lock(lock_name, timeout)
+        else
+          !!session_store.add?(lock_name)
+        end
       end
 
       def release_lock(lock_name)
-        !!session_store.delete?(lock_name)
+        if redlock_enabled?
+          redlock_client.unlock(lock_name)
+        else
+          !!session_store.delete?(lock_name)
+        end
       end
 
       def release_all!
@@ -19,6 +37,14 @@ module BeachSand
 
       def session_store
         @session_store ||= Set.new([])
+      end
+
+      def redlock_enabled?
+        @redlock_enabled ||= !ENV.fetch("REDIS_URL", "").empty?
+      end
+
+      def redlock_client
+        @redlock_client ||= Redlock::Client.new([ ENV["REDIS_URL"] ]) if redlock_enabled?
       end
     end
   end
